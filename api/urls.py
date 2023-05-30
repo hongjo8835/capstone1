3d922ac37dcba6737f46b2f0d53f37f3106ad13a
Hyeongmin Kim <50308836@hanmail.net>
2023-05-30 10:07:41 +0900

    views (delete, update)ingredients

--- api/urls.py
+++ api/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path("login/", views.login),
     path("get/", views.get_foodlist_info),
     path("putingredient/", views.put_ingredient),
-    path("checkid/", views.check_id)
+    path("checkid/", views.check_id),
+    path("delingredients/", views.delete_ingredients),
+    path("update_ingredient/", views.update_ingredient)
 ]
