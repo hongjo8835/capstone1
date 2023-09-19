51a739c6e4a553b3daa04edfd5fb934cf5b9804b
hongjo8835 <50308836@hanmail.net>
2023-09-19 10:11:44 +0900

    url edit

--- api/urls/food_urls.py
+++ api/urls/food_urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from ..views import barnum_return, get_foodlist_info, put_ingredient, delete_ingredients, update_ingredient
 
 urlpatterns = [
-    path("food/barnumretrun/", barnum_return),
-    path("food/get/", get_foodlist_info),
-    path("food/putingredient/", put_ingredient),
-    path("food/delingredients/", delete_ingredients),
-    path("food/update_ingredient/", update_ingredient),
+    path("barnumretrun/", barnum_return),
+    path("get/", get_foodlist_info),
+    path("put_ingredient/", put_ingredient),
+    path("del_ingredients/", delete_ingredients),
+    path("updt_ingredient/", update_ingredient),
 ]
--- api/urls/user_urls.py
+++ api/urls/user_urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from ..views import user_join, login, check_id
 
 urlpatterns = [
-    path("user/join/", user_join),
-    path("user/login/", login),
-    path("user/checkid/", check_id),
+    path("join/", user_join),
+    path("login/", login),
+    path("checkid/", check_id),
 ]
