95ca8c83209f267f3128aa540567a7376573735b
hongjo8835 <50308836@hanmail.net>
2023-09-19 10:02:17 +0900

    __init__.py edit, add

--- api/urls/__init__.py
+++ api/urls/__init__.py
@@ -1,9 +1,7 @@
-from django.urls import path, include
-
+from django.urls import include, path
 
 urlpatterns = [
     path('post/', include('api.urls.board_urls')),
     path('food/', include('api.urls.food_urls')),
     path('user/', include('api.urls.user_urls')),
-
 ]
--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,4 +1,3 @@
 from .board_views import *
-from .commnet_views import *
 from .food_views import *
 from .user_views import *
