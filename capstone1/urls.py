4d1541b27105e8713c706d172de64d70e13db865
hongjo8835 <50308836@hanmail.net>
2023-11-09 15:43:21 +0900

    refresh_token 수정

--- capstone1/urls.py
+++ capstone1/urls.py
@@ -34,5 +34,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include('rest_framework.urls')),
     path("api/token/", TokenObtainPairView.as_view()),
-    path("api/token/refresh/", CustomTokenRefreshView.as_view()),
+    path("api/token/refresh/", TokenRefreshView.as_view()),
 ]
